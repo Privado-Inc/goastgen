3af99b11444a1e8891ce820ddffeb2a34ff4b72a
Pandurang Patil <pandurang.patil@privado.ai>
2023-06-13 13:34:52 +0530

    Release workflow for executable binary
    1. Made changes to try executable binary without cgo
    2. Release workflow changes to build binaries for os and architecture
    combinations

--- main.go
+++ main.go
@@ -1,32 +1,12 @@
 package main
 
-import "C"
 import (
 	"fmt"
 	"os"
 	"privado.ai/goastgen/goastgen"
 )
 
-//export ParseAstFromSource
-func ParseAstFromSource(filename *C.char, src *C.char) *C.char {
-	resultJson := goastgen.ParseAstFromSource(C.GoString(filename), C.GoString(src))
-	return C.CString(resultJson)
-}
-
-//export ParseAstFromDir
-func ParseAstFromDir(dir *C.char) *C.char {
-	resultJson := goastgen.ParseAstFromDir(C.GoString(dir))
-	return C.CString(resultJson)
-}
-
-//export ParseAstFromFile
-func ParseAstFromFile(file *C.char) *C.char {
-	resultJson := goastgen.ParseAstFromFile(C.GoString(file))
-	return C.CString(resultJson)
-}
-
 func main() {
-	fmt.Println("Trial goastgen")
 	args := os.Args[1:]
 	path := args[0]
 	resultJson := goastgen.ParseAstFromFile(path)
